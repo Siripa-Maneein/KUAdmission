1a1fce53b0c67cf9f0b101dcb227c60ed410cecd
Siripa-Maneein <siripa.m@ku.th>
2024-04-12 23:29:04 +0700

    remove next.js

--- l4-app/frontend/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/siripamaneein/Desktop/ll/l4-app/frontend/app/layout\":[\"static/media/c9a5bc6a7c948fb0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
--- l4-app/frontend/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"eLg/TWY/dgoSyInc9trb43jgY++r7gysgNResn42A78=\"\n}"
